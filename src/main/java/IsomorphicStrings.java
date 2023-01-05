e4c89b3d523a037daa6027465658884028f1daa4
Maria Paz <pazrodriguez612@gmail.com>
2023-01-04 23:58:02 -0600

    Isomorphic Strings

---
+++ src/main/java/IsomorphicStrings.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!map.containsKey(s.charAt(i)) && !map.containsValue(t.charAt(i)))
+                map.put(s.charAt(i), t.charAt(i));
+
+            if (map.getOrDefault(s.charAt(i), ' ') != t.charAt(i))
+                return false;
+        }
+        return true;
+    }
+}
