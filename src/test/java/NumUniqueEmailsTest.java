17116eb1f1261e48d3449e3b5e33005d44f3487b
Maria Paz <pazrodriguez612@gmail.com>
2023-01-04 23:56:42 -0600

    Unique emails Test

--- src/test/java/NumUniqueEmailsTest.java
+++ src/test/java/NumUniqueEmailsTest.java
@@ -1,5 +1,33 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumUniqueEmailsTest {
+    @Test
+    void dupEmails() {
+        int ans = new NumUniqueEmails().numUniqueEmails(new String[]{"Hey@gmail.com", "Hey@gmail.com"});
 
+        assertEquals(1, ans);
+    }
+
+    @Test
+    void noDupEmails() {
+        int ans = new NumUniqueEmails().numUniqueEmails(new String[]{"Hey@gmail.com", "Hello@gmail.com"});
+
+        assertEquals(2, ans);
+    }
+
+    @Test
+    void diffDomain() {
+        int ans = new NumUniqueEmails().numUniqueEmails(new String[]{"Hey@hotmail.com", "Hey@gmail.com"});
+
+        assertEquals(2, ans);
+    }
+
+    @Test
+    void sameWithNoAt() {
+        int ans = new NumUniqueEmails().numUniqueEmails(new String[]{"Hey@gmail.com", "He@ygmail.com"});
+
+        assertEquals(2, ans);
+    }
 }
